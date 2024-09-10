6a3e5d7d17d2bddb390f577bf93c171f8b6a296b
brunokmarga <48949533+brunokmarga@users.noreply.github.com>
2024-09-10 12:39:24 -0300

    Update acesso.py
    Atualização para filtro com variável auxiliar

--- teste_banco_dados/acesso.py
+++ teste_banco_dados/acesso.py
@@ -1,17 +1,23 @@
 import pyodbc
 
+aux = 177
+
 # String de conexão com o banco de dados
 conn = pyodbc.connect('Driver={SQL Server};'
-'Server=ts.daraujo.local;'
-'Database=CVRBI02;'
-'UID=gsv.novoacesso;'
-'PWD=ESgsvxx11*')
+                      'Server=ts.daraujo.local;'
+                      'Database=CVRBI02;'
+                      'UID=gsv.novoacesso;'
+                      'PWD=ESgsvxx11*')
 
 # Criar um objeto cursor
 cursor = conn.cursor()
 
-# Comando SQL a executar
-cursor.execute('SELECT * FROM Clientes')
+# Comando SQL a executar, usando um placeholder (?) para o valor
+cursor.execute('SELECT * FROM Planta WHERE clientes_codigo = ?', aux)
+
+# Obter os nomes das colunas
+columns = [column[0] for column in cursor.description]
+print("Nomes das colunas:", columns)
 
 # Buscar todos os resultados
 for row in cursor:
